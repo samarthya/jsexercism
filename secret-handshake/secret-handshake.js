9b09e9b4e78c7ed6903cbfe56b696aa23d3bcf9d
Saurabh Sharma <saurabh777@gmail.com>
2020-01-11 23:38:12 +0530

    Second

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -1,57 +1,27 @@
-//
-// This is only a SKELETON file for the 'Secret Handshake' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-function binaryOptimized(number) {
-  var binaryRep = [];
-  do {
-    binaryRep.push(parseInt(number%2));
-    number /= 2;
-  } while (parseInt(number) > 0)
-  return binaryRep;
-}
-
-
-function getCommand(pseudo, arr) {
-  switch(pseudo) {
-    case 1:
-      arr.push ("wink");
-      break;
-    case 10:
-      arr.push ("double blink");
-      break;
-    case 100:
-      arr.push ("close your eyes");
-      break;
-    case 1000:
-      arr.push ("jump");
-      break;
-    case 10000:
-      arr = arr.reverse();
+export const commands = (code) => {
+  if (isNaN(parseInt(code))) {
+    throw new Error('Handshake must be a number');
   }
-  return arr;
-}
+  let handshake = [];
+  if (code & 1) {
+    handshake.push ('wink');
+  }
 
-/**
- * 1 = wink
- * 10 = double blink
- * 100 = close your eyes
- * 1000 = jump
- * 10000 = Reverse the order of the operations in the secret handshake.
- */
-/**
- * 
- * @param {*} input 
- */
-export const commands = (input) => {
-    //var rep = binary(input);
-    var rep = binaryOptimized(input);
-    var commands = [];
+  if (code & 2) {
+    handshake.push ('double blink');
+  }
 
-    rep.map((element, i) => {
-      getCommand(element * Math.pow(10, i), commands)
-    });
+  if (code & 4) {
+    handshake.push ('close your eyes');
+  }
 
-    return commands;
+  if (code & 8) {
+    handshake.push ('jump');
+  }
+
+  if (code & 16) {
+    handshake.reverse();
+  }
+
+  return handshake;
 };
