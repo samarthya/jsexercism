cdef040a85b826b3c9e83de36b9f5867daa6f846
Saurabh Sharma <saurabh777@gmail.com>
2020-01-12 12:32:46 +0530

    Jan 12 2020

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -3,10 +3,7 @@ const HANDSHAKES = ["wink", "double blink", "close your eyes", "jump"];
 export const commands = (n) => {
   if (typeof n !== "number") {
     throw new Error("Handshake must be a number");
-  }
-
-  // console.log('TEST', n & (1 << index));
-  
+  }  
   const result = HANDSHAKES.filter((signal, index) => n & (1 << index));
 
   if (n & (1 << HANDSHAKES.length)) {
